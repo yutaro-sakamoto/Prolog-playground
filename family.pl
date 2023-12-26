407c026ce66bffb806a84d8084a824e96b1221b3
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-12-27 01:42:37 +0900

    [Add]: add rules of family

--- family.pl
+++ family.pl
@@ -3,5 +3,20 @@ father(jiro, saburo).
 father(jiro, shiro).
 father(jiro, hanako).
 father(saburo, koichi).
+father(koichi, yuki).
 mother(yoko, jiro).
 mother(kyoko, koichi).
+mother(yuka, yuki).
+mother(sachiko, saburo).
+
+parent(P, C) :- father(P, C).
+parent(P, C) :- mother(P, C).
+
+grandfather(G, C) :- father(G, P), parent(P, C).
+grandmother(G, C) :- mother(G, P), parent(P, C).
+grandparent(G, C) :- grandfather(G, C).
+grandparent(G, C) :- grandmother(G, C).
+
+child(C, P) :- parent(P, C).
+grandchild(C, G) :- grandparent(G, C).
+sibling(X, Y) :- parent(P, X), parent(P, Y).
